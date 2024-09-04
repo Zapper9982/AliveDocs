39cbbfb18d611a86b3bee97e520f5325080f86ec
Zapper9982 <144029909+Zapper9982@users.noreply.github.com>
2024-09-05 03:32:58 +0530

    initial commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignIn } from '@clerk/nextjs'
+
+
+const SignInPage = () => {
+  return (
+   <main className='auth-page'>
+    <SignIn/>
+
+   </main>
+  )
+}
+
+export default SignInPage
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignUp} from '@clerk/nextjs'
+
+
+const SignUpPage = () => {
+  return (
+   <main className='auth-page'>
+    <SignUp/>
+
+   </main>
+  )
+}
+
+export default SignUpPage
