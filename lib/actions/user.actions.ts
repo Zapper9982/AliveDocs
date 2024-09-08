da8716611c4418f84bddeb9224f8361aae5b5c69
Zapper9982 <144029909+Zapper9982@users.noreply.github.com>
2024-09-08 14:49:38 +0530

    adding Mention feature

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,7 @@
 'use server';
-import { clerkClient } from "@clerk/nextjs/server";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
     try {
@@ -22,4 +23,22 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
       console.log(`Error fetching users: ${error}`);
     }
   }
-  
+
+  export const getDocumentUsers = async ({roomId , currentUser, text}:{roomId:string , currentUser:string,text:string})=>{
+     try {
+        const room = await liveblocks.getRoom(roomId);
+        const users  = Object.keys(room.usersAccesses).filter((email)=> email !== currentUser);
+        if(text.length){
+            const lowercaseText = text.toLowerCase();
+
+            const filteredUsers = users.filter((email:string)=>email.toLowerCase().includes(lowercaseText));
+
+            return parseStringify(filteredUsers);
+        }
+        return parseStringify(users);
+     } catch (error) {
+        console.log(`Error fetching users for document ${roomId} : ${error}`);
+        
+     }
+
+  }
